ce3a8328bd8a5487c11ec3e5831137e152b67e39
Nicolas Andreis <jnandreis@outlook.com>
2023-10-14 16:55:07 -0300

    preentrega2

--- main.js
+++ main.js
@@ -5,35 +5,53 @@ function saludar() {
 //invoco a la funcion saludar
 saludar();
 
-//declaracion de variables globales por el scoope
-let muzzarella = "1-pizza muzzarella  $2530";
-let napolitana = "2-pizza napolitana $2750";
-let fugazza = "3-pizza fugazza  $3000";
+// Definición de la clase Producto para representar las pizzas
+class Producto{
+    constructor (nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+// Creación de un array para almacenar las variedades de pizzas
+const productos = [];
+
+// Agregar objetos Producto al array productos
+productos.push(new Producto("pizza muzzarella", 2530));
+productos.push(new Producto("pizza napolitana", 2750));
+productos.push(new Producto("pizza fugazza", 3000));
 
 //declaro una funcion para mostrar variedades con alert y variables
 function mostrarVariedades() {
     alert("te mostraremos nuestras variedades");
 
-    alert(`
-    ${muzzarella}
-    ${napolitana}
-    ${fugazza}`);
+    //utilizo forEach para recorrer el array
+    productos.forEach(pizza => {
+        alert(`${pizza.nombre} $${pizza.precio}`)
+    });
 }
 
+//llamo a la funcion mostrar Variedades
 mostrarVariedades();
 
 let continuarComprando = true; //paara que entre en el while
 let compras = ""; //declaro una variable para almacenar las compras
 let totalComprado = 0; //declaro una variable en cero para almacenar el valor de las pizzas compradas
 
+// Inicio de un bucle while para continuar comprando
 while (continuarComprando) {
 
-    let pizzaSeleccionada = parseInt(prompt(`
-        Ingrese el número correspondiente a la pizza que desea comprar:
-        1. Muzzarella   $2530
-        2. Napolitana   $2750
-        3. Fugazza      $3000
-        Para salir, escriba "ESC"`));
+// Creación de un mensaje que muestra las opciones de pizza disponibles
+let options = "Ingrese el número correspondiente a la pizza que desea comprar:\n";
+
+// Recorriendo el array de productos para mostrar las opciones de pizza
+for (let i = 0; i < productos.length; i++) {
+    options += `${i + 1}. ${productos[i].nombre} $${productos[i].precio}\n`;
+}
+
+options += "Para salir, escriba 'ESC'";
+
+//parseo el string para convertirlo en un numero
+let pizzaSeleccionada = parseInt(prompt(options));
 
     switch (pizzaSeleccionada) {
         case 1:
