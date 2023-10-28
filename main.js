a17b1c08e53300b6c4adf28699a30a25632342b0
Nicolas Andreis <jnandreis@outlook.com>
2023-10-28 12:21:42 -0300

    3rapreentrega

--- main.js
+++ main.js
@@ -1,117 +1,197 @@
-//declaro la funcion saludar
-function saludar() {
-    alert("bienvenido a nuestra pizzeria");
-}
-//invoco a la funcion saludar
-saludar();
+// Seleccionar el contenedor del carrito
+const carritoContainer = document.getElementById("carrito");
 
-// Definición de la clase Producto para representar las pizzas
-class Producto {
-    constructor(nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
+// Declarar un array con objetos
+const pizzas = [
+    {
+        id: 1,
+        nombre: "muzzarella",
+        precio: 2910,
+        imagen: "./imagenes/pizzas/muzzarella.jpg",
+    },
+    {
+        id: 2,
+        nombre: "fuggazza",
+        precio: 2910,
+        imagen: "./imagenes/pizzas/fuggazza.jpg",
+    },
+    {
+        id: 3,
+        nombre: "napolitana",
+        precio: 2910,
+        imagen: "./imagenes/pizzas/napolitana.jpg",
+    },
+    {
+        id: 4,
+        nombre: "especial",
+        precio: 2910,
+        imagen: "./imagenes/pizzas/especial.jpg",
+    },
+];
+
+// Inicializar el carrito desde el almacenamiento local
+let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+// Función para guardar el carrito en el almacenamiento local
+function guardarCarritoEnLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+// Función para agregar tarjetas al contenedor
+function agregarTarjetas(container, productos) {
+    for (const producto of productos) {
+        let contenedor = document.createElement("div");
+        contenedor.classList.add("card");
+        contenedor.innerHTML = `
+            <img src="${producto.imagen}" alt="">
+            <div class="card-texto">
+                <p>${producto.nombre}</p>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="card-iconos">
+                <div class="contador">
+                    <img src="./imagenes/shopping/sum.png" alt="+" class="sum-btn" data-product-id="${producto.id}">
+                </div>
+            </div>
+        `;
+        container.appendChild(contenedor);
     }
 }
-// Creación de un array para almacenar las variedades de pizzas
-const productos = [];
 
-// Agregar objetos Producto al array productos
-productos.push(new Producto("pizza muzzarella", 2530));
-productos.push(new Producto("pizza napolitana", 2750));
-productos.push(new Producto("pizza fugazza", 3000));
+// Seleccionar el contenedor de pizzas
+const contenedorPizzas = document.querySelector("#pizzas");
 
-//declaro una funcion para mostrar variedades con alert y variables
-function mostrarVariedades() {
-    alert("te mostraremos nuestras variedades");
+// Agregar tarjetas al contenedor
+agregarTarjetas(contenedorPizzas, pizzas);
 
-    //utilizo forEach para recorrer el array
-    productos.forEach(pizza => {
-        alert(`${pizza.nombre} $${pizza.precio}`)
+// Agregar un evento a cada botón de suma
+const sumButtons = document.querySelectorAll(".sum-btn");
+sumButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const productoId = parseInt(event.target.getAttribute("data-product-id"));
+
+        // Buscar el producto por ID en las pizzas
+        const producto = pizzas.find(p => p.id === productoId);
+
+        if (producto) {
+            // Verificar si el producto ya está en el carrito
+            const productoEnCarrito = carrito.find(item => item.id === producto.id);
+
+            if (productoEnCarrito) {
+                // Si el producto ya está en el carrito, aumenta la cantidad y recalcula el precio total
+                productoEnCarrito.cantidad += 1;
+                productoEnCarrito.precioTotal = productoEnCarrito.cantidad * productoEnCarrito.precio;
+            } else {
+                // Si el producto no está en el carrito, agrégalo con cantidad 1 y calcula el precio total
+                const productoConCantidad = { ...producto, cantidad: 1, precioTotal: producto.precio };
+                carrito.push(productoConCantidad);
+            }
+
+            // Guardar el carrito actualizado en el almacenamiento local
+            guardarCarritoEnLocalStorage();
+
+            // Actualizar la vista del carrito
+            mostrarCarritoEnHTML();
+        }
     });
-}
+});
 
-//llamo a la funcion mostrar Variedades
-mostrarVariedades();
-
-let continuarComprando = true; //paara que entre en el while
-let compras = ""; //declaro una variable para almacenar las compras
-const carrito = []; //array en cero para almacenar los precios para el carrito
-
-function agregarAlCarrito(precio) {
-    carrito.push(precio);
-}
-
-// Función para sumar los elementos del carrito
-function sumarCarrito(carrito) {
+// Función para calcular el total del carrito
+function calcularTotalCarrito() {
     let total = 0;
-    for (let i = 0; i < carrito.length; i++) {
-        total += carrito[i];
+    for (const producto of carrito) {
+        total += producto.precioTotal;
     }
     return total;
 }
 
-// Inicio de un bucle while para continuar comprando
-while (continuarComprando) {
+// Función para mostrar el carrito en el HTML
+function mostrarCarritoEnHTML() {
+    // Limpiar el contenido anterior del carrito
+    carritoContainer.innerHTML = `
+    <div id="container-numerito">
+        <h2>Mi carrito</h2>
+        <p id="numerito">0</p> 
+    </div>    
+    
+    `;
 
-    // Creación de un mensaje que muestra las opciones de pizza disponibles
-    let options = "Ingrese el número correspondiente a la pizza que desea comprar:\n";
+    // Iterar a través de los productos en el carrito y mostrarlos con cantidades
+    carrito.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.classList.add('carrito-item');
 
-    // Recorriendo el array de productos para mostrar las opciones de pizza
-    for (let i = 0; i < productos.length; i++) {
-        options += `${i + 1}. ${productos[i].nombre} $${productos[i].precio}\n`;
-    }
+        productoDiv.innerHTML = `
+            <p>${producto.nombre} x ${producto.cantidad}</p>
+            <p>Total: $${producto.precioTotal}</p>
+        `;
+        carritoContainer.appendChild(productoDiv);
+    });
 
-    options += "Para salir, escriba 'ESC'";
-
-    //parseo el string para convertirlo en un numero
-    let pizzaSeleccionada = parseInt(prompt(options));
-
-    switch (pizzaSeleccionada) {
-        case 1:
-            compras += "Pizza de muzzarella\n";
-            agregarAlCarrito(2530);
-            alert("Pizza de muzzarella comprada");
-            break;
-
-        case 2:
-            compras += "Pizza de napolitana\n";
-            agregarAlCarrito(2750);
-            alert("Pizza de napolitana comprada");
-            break;
-
-        case 3:
-            compras += "Pizza de fugazza\n";
-            agregarAlCarrito(3000);
-            alert("Pizza de fugazza comprada");
-            break;
-
-        case "ESC":
-            alert("Saliendo...");
-            continuarComprando = false; // Establece la bandera para salir del bucle
-            break;
-
-        default:
-            alert("Ingresó una opción incorrecta, vuelva a intentar.");
-            break;
-    }
-
-    if (continuarComprando) {
-        continuarComprando = confirm("¿Desea seguir comprando?"); // devuelve true o false
-    }
+    // Calcular y mostrar el total del carrito
+    const totalCarrito = document.createElement('div');
+    totalCarrito.classList.add('carrito-total');
+    totalCarrito.innerHTML = `
+        <p>Total del Carrito: $${calcularTotalCarrito()}</p>
+    `;
+    carritoContainer.appendChild(totalCarrito);
+    actualizarNumerito();
 }
 
+// Llamar a la función para mostrar el carrito en el HTML
+mostrarCarritoEnHTML();
 
-// Llamar a la función para obtener el total del carrito
-const totalComprado = sumarCarrito(carrito);
+// ...
 
-alert("Gracias por su compra!");
-alert(`
-pizzas compradas:
-${compras}
-total del pedido  
-$${totalComprado}
-`);
+// Función para vaciar el carrito y actualizar la vista
+function vaciarCarrito() {
+    // Vaciar el carrito (establecerlo como un array vacío)
+    carrito = [];
 
-// Para llamar a la función y comenzar la selección de pizzas
-seleccionar();
+    // Guardar el carrito vacío en el almacenamiento local
+    guardarCarritoEnLocalStorage();
 
+    // Actualizar la vista del carrito
+    mostrarCarritoEnHTML();
+    actualizarNumerito();
+}
+
+// Agregar un evento al botón "Vaciar Carrito"
+const vaciarCarritoButton = document.getElementById("vaciarCarrito");
+vaciarCarritoButton.addEventListener("click", () => {
+    // Llamar a la función para vaciar el carrito
+    vaciarCarrito();
+    
+});
+
+// ...
+// Agregar un controlador de eventos al botón "Comprar Carrito"
+const comprarCarritoButton = document.getElementById("comprarCarrito");
+comprarCarritoButton.addEventListener("click", () => {
+    // Mostrar un mensaje de "Compra Exitosa" utilizando alert
+    alert("Compra Exitosa");
+
+    // Vaciar el carrito después de la compra
+    vaciarCarrito();
+
+    // Actualizar la vista del carrito
+    mostrarCarritoEnHTML();
+
+    actualizarNumerito();
+});
+
+function actualizarNumerito() {
+    let nuevoNumerito = carrito.reduce(
+        (acc, producto) => acc + producto.cantidad,
+        0
+    );
+
+    console.log("Nuevo numerito:", nuevoNumerito);
+
+    const numerito = document.getElementById("numerito");
+    console.log("Elemento numerito:", numerito);
+
+    numerito.innerText = nuevoNumerito; // Actualiza el contenido del elemento numerito con el nuevo valor
+}
+
+actualizarNumerito();
